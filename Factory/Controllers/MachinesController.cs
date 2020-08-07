0fce5dcacd7da3a0d6ae1375d39ba2e5978a12e1
Taylor Somers <tsomers@yandex.com>
2020-08-07 14:30:59 -0400

    Add Index HTTPGet action

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,24 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Machines.ToList());
+    }
+  }
+}
