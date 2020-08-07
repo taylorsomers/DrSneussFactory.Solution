a8ae0123d17b4096b9cd64ae5b5aefe0435d6437
Taylor Somers <tsomers@yandex.com>
2020-08-07 19:41:11 -0400

    Add Edit HTTPGet action

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -46,5 +46,11 @@ namespace Factory.Controllers
         .FirstOrDefault(machine => machine.MachineId == id);
         return View(thisMachine);
     }
+
+    public ActionResult Edit(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
   }
 }
