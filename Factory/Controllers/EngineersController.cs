6170000f32a8a6d7963251e8d13bf68203dd0c71
Taylor Somers <tsomers@yandex.com>
2020-08-07 13:00:58 -0400

    Add Index HTTPGet action

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,25 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+  }
+}
